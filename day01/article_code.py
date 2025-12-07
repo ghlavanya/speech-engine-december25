84914e6f98dddd68d4a83d9083145c0a98081314
ghlavanya <lavanyachd2006@gmail.com>
2025-12-07 13:35:30 +0530

    added new article_code file

---
+++ day01/article_code.py
@@ -0,0 +1,15 @@
+from scipy.io import wavfile
+import numpy as np
+
+audio_path = r"C:\Users\lavan\OneDrive\Desktop\dev\spectograms\0_jackson_21.wav"
+
+sr, samples = wavfile.read(audio_path)
+
+print("SciPy sample rate:", sr)
+print("Raw samples dtype:", samples.dtype)
+print("First 10 samples:", samples[:10])
+
+# Normalize if needed
+if samples.dtype != np.float32:
+    samples = samples.astype(np.float32) / np.max(np.abs(samples))
+    print("Normalized samples dtype:", samples.dtype)
